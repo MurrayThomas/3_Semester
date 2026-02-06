a0a7e9b3d8c9e6f19ab066ceac922cac1fcbe0f2
Thomas Murray <thomasmurray897@gmail.com>
2026-02-06 12:56:50 +0100

    ObjectInitialization

---
+++ Helloproject/ObjectInitialization/Model/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectInitialization.Model
+{
+
+    public class Person
+    {
+        private string? name;
+        private DateTime? birthdate;
+
+        public Person()
+        {
+        }
+
+        public Person(string inName) {
+            name = inName;
+        }
+
+        public Person(string inName, DateTime? birthdate)
+        {
+            this.name = inName;
+            this.birthdate = birthdate;
+        }
+
+        public string Name { get { return name; } }
+
+
+    }
+}
---
+++ Helloproject/ObjectInitialization/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ObjectInitialization.Model;
+
+Console.WriteLine("Hello, World!");
+
+Person p1 = new Person();
+Person p2 = new Person("Thomas Murray");
+Person p3 = new Person("Jens Jensen" + new DateTime(1999, 8, 31));
+
+Console.WriteLine("Name: " + p1.Name);
+Console.WriteLine("Name: " + p2.Name);
+Console.WriteLine("Name: " + p3.Name + " ");
+
+
